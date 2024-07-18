d0af8540fc380e2bb08780b9a3c18a9b26d2d4ec
Alexandra_V <alexandra.vukits82@gmail.com>
2024-07-18 11:19:13 +0200

    Added the functionality for the select in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,53 @@
 function updateLosAngelesTime() {
   let losAngelesVariable = document.querySelector("#los-angeles");
-  let losAngelesDateVariable = losAngelesVariable.querySelector(".date");
-  let losAngelesTimeVariable = losAngelesVariable.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
-  losAngelesDateVariable.innerHTML = `Today is ${moment().format(
-    "dddd, MMMM Do YYYY"
-  )}`;
-  losAngelesTimeVariable.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small]"
-  );
+  if (losAngelesVariable) {
+    let losAngelesDateVariable = losAngelesVariable.querySelector(".date");
+    let losAngelesTimeVariable = losAngelesVariable.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+    losAngelesDateVariable.innerHTML = `Today is ${moment().format(
+      "dddd, MMMM Do YYYY"
+    )}`;
+    losAngelesTimeVariable.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small]"
+    );
+  }
 }
 setInterval(updateLosAngelesTime, 1000);
 
 function updateParisTime() {
   let parisVariable = document.querySelector("#paris");
-  let parisDateVariable = parisVariable.querySelector(".date");
-  let parisTimeVariable = parisVariable.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisDateVariable.innerHTML = `Today is ${moment().format(
-    "dddd, MMMM Do YYYY"
-  )}`;
-  parisTimeVariable.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small]");
+  if (parisVariable) {
+    let parisDateVariable = parisVariable.querySelector(".date");
+    let parisTimeVariable = parisVariable.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateVariable.innerHTML = `Today is ${moment().format(
+      "dddd, MMMM Do YYYY"
+    )}`;
+    parisTimeVariable.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small]"
+    );
+  }
 }
 setInterval(updateParisTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesVariable = document.querySelector("#cities");
+  citiesVariable.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">Today is ${cityTime.format(
+              "dddd, MMMM Do YYYY"
+            )}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
+let citiesSelectVariable = document.querySelector("#city");
+citiesSelectVariable.addEventListener("change", updateCity);
